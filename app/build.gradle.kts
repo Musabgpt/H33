2956bd3b8aa89d64f7cf25ca3e86b9c6d912bbe1
Musabgpt <mgpt6zu@gmail.com>
2026-09-19 19:18:37 +0100

    Target stable Android API 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.musab.aragpt2"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.musab.aragpt2"
         minSdk = 28
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 2
         versionName = "0.2.0"
 
